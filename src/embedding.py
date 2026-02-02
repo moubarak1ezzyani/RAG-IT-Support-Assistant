2dc6d9d84e58e94a1fbd223c2ebf2c3f59a6a0cb
moubarak1ezzyani <moubarak.ezzyani@gmail.com>
2026-02-02 12:02:43 +0100

    comment the old version and init the new file

---
+++ src/embedding.py
@@ -0,0 +1,68 @@
+# embedding file
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+
+
+
+#--------------------------------------
+# # Model loader (BGE-M3)
+# import os
+# from huggingface_hub import InferenceClient
+# from ingestion import chunks
+# from dotenv import load_dotenv
+# from langchain_huggingface import HuggingFaceEmbeddings
+# from langchain_chroma import Chroma
+
+# load_dotenv()
+# # --- exetrnal model
+# # -> var
+# hf_token=os.getenv("HF_TOKEN")
+# embedding_model_id="BAAI/bge-m3"
+
+# client = InferenceClient(
+#     provider="hf-inference",
+#     # api_key=os.environ["HF_TOKEN"],
+#     api_key=hf_token
+# )
+# text_source=input("Type a sentence to start embedding:")
+
+# # --- convert to vectors
+# embeddings=HuggingFaceEmbeddings(model_name=embedding_model_id)
+# query_result=embeddings.embed_documents(text_source)  # query : embed_query() | doc : embed_documents()
+# print(str(query_result)[:100])  # 100 characters
+
+
+# # --- chroma db
+
+
+# # --- searching for similarity
+# for chunk in chunks:
+#     result = client.sentence_similarity(
+#         {
+#         "source_sentence": text_source,
+#         "sentences": chunks
+#     },  
+#         model=embedding_model_id,
+#     )
+
+
+
+
+
+
+# vectors = client.feature_extraction(
+#     # "Today is a sunny day and I will get some ice cream.",
+#     text_source,
+#     model=EMBEDDING_MODEL_ID
+# )
+
+# print("--- Generated vector ---")
+# print(f"dimensions : {len(vectors)} ")
+# print(f"preview {vectors[:5]}")
+
+# embedding_model = HuggingFaceEndpointEmbeddings(
+#         model=EMBEDDING_MODEL_ID,
+#         task="feature-extraction", 
+#         huggingfacehub_api_token=hf_token
+#     )
+# #----------------------------------------------------
+
