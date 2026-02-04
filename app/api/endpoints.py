81e9bea8f40eae7c8e8e8d45a762978148015267
moubarak1ezzyani <moubarak.ezzyani@gmail.com>
2026-02-04 16:43:53 +0100

    feat(api): implement create_user endpoint & get_db dependency

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -1,1 +1,31 @@
 # Tes routes (POST /query, POST /auth, etc.)
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from ..db import models
+from ..db import schemas
+from ..db.database import SessionLocal
+
+router = APIRouter()
+
+# Dependency to get DB session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.post("/users/", response_model=schemas.UserResponse)
+def create_user(user_input: schemas.UserCreate, db: Session = Depends(get_db)):
+    # schema -> models
+    db_user = models.User(
+        email=user_input.email, 
+        hashed_password=user_input.password + "fakehash" 
+    )
+    
+    # Save to db
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    
+    return db_user
