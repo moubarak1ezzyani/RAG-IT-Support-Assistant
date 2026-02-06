c937788908dca9adc119500a3e8a83bc90800710
moubarak1ezzyani <moubarak.ezzyani@gmail.com>
2026-02-07 00:55:16 +0100

    Refactor database connection to use central config
    Moved environment variable loading and db_url construction to app.core.config, simplifying database.py to focus only on session management.

--- app/db/database.py
+++ app/db/database.py
@@ -1,20 +1,9 @@
 # Connexion 
-from dotenv import load_dotenv
-import os
+from app.core.config import db_url 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base, Session
 from typing import Annotated
 from fastapi import Depends
-# var
-load_dotenv()
-DB_USER=os.getenv("db_user_env")
-DB_PASSWORD=os.getenv("db_password_env")
-DB_HOST=os.getenv("db_host_env")
-DB_PORT=os.getenv("db_port_env","5432")
-DB_NAME=os.getenv("db_name_env")
-
-# link db : protocol://username:password@host:port/database_name
-db_url = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # engine
 engine = create_engine(db_url)
