7639618c26f3e80b4f651c1eddbcf71e65b6f38b
moubarak1ezzyani <moubarak.ezzyani@gmail.com>
2026-02-07 00:26:38 +0100

    Refactor database config and add session dependency
    1. Standardized DB environment variables to uppercase (DB_USER, DB_HOST, etc.). 2. Implemented get_db generator for managing database sessions. 3. Added db_dependancy for cleaner FastAPI injection.

--- app/db/database.py
+++ app/db/database.py
@@ -2,18 +2,19 @@
 from dotenv import load_dotenv
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
+from typing import Annotated
+from fastapi import Depends
 # var
 load_dotenv()
-db_user=os.getenv("db_user_env")
-db_password=os.getenv("db_password_env")
-db_host=os.getenv("db_host_env")
-db_port=os.getenv("db_port_env","5432")
-db_name=os.getenv("db_name_env")
+DB_USER=os.getenv("db_user_env")
+DB_PASSWORD=os.getenv("db_password_env")
+DB_HOST=os.getenv("db_host_env")
+DB_PORT=os.getenv("db_port_env","5432")
+DB_NAME=os.getenv("db_name_env")
 
 # link db : protocol://username:password@host:port/database_name
-db_url = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+db_url = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # engine
 engine = create_engine(db_url)
@@ -25,3 +26,11 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
+def get_db():
+    db=SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependancy=Annotated[Session, Depends(get_db)]
