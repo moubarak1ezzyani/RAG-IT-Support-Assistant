a70ae4bd5d39ad5eb5953f30755641feb777dee0
moubarak1ezzyani <moubarak.ezzyani@gmail.com>
2026-02-07 20:35:32 +0100

    refactor : Improved error checking and cleaned up the console logs

--- app/rag/ingestion.py
+++ app/rag/ingestion.py
@@ -11,6 +11,10 @@ pdf_path = os.path.join(script_dir, "..", "data", "The-IT-Support-Handbook.pdf")
 
 # --- function
 def ingest_document():
+    # path checking
+    if not os.path.exists(pdf_path):
+        raise FileNotFoundError(f"❌ PDF not found at path: {pdf_path}")
+    
     print(f"starting digital typist for {pdf_path}")
 
     try:
@@ -27,19 +31,19 @@ def ingest_document():
         chunks=splitter.split_documents(raw_pages)
         print(f"split document into {len(chunks)} chunks")
 
-        # --- INSPECT
-        print("--- 1st Chunk ---")
-        print(chunks[0].page_content)
-        print(f"metadata : {chunks[0].metadata}")
+        # # --- INSPECT
+        # print("--- 1st Chunk ---")
+        # print(chunks[0].page_content)
+        # print(f"metadata : {chunks[0].metadata}")
         
-        print("--- 2nd Chunk ---")
-        print(chunks[1].page_content)
-        print(f"metadata : {chunks[1].metadata}")
+        # print("--- 2nd Chunk ---")
+        # print(chunks[1].page_content)
+        # print(f"metadata : {chunks[1].metadata}")
         
     except FileNotFoundError:
         print(f"I couldn't find {pdf_path}")
 
     except Exception as e:
-        print(f"an error occured {e}")
+        print(f"❌ an error occured {e}")
 
     return chunks
