98df5913bf5f6c553ddf3ad444747a907c46930b
moubarak1ezzyani <moubarak.ezzyani@gmail.com>
2026-02-08 02:02:16 +0100

    clean spaces

--- app/core/config.py
+++ app/core/config.py
@@ -2,8 +2,9 @@
 from dotenv import load_dotenv
 import os
 
-# var
 load_dotenv()
+
+# db    
 DB_USER=os.getenv("db_user_env")
 DB_PASSWORD=os.getenv("db_password_env")
 DB_HOST=os.getenv("db_host_env")
@@ -12,10 +13,6 @@ DB_NAME=os.getenv("db_name_env")
 
 # link db : protocol://username:password@host:port/database_name
 db_url = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # --- rag
 # ingestion
